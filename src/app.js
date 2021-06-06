a978fdc0e0cdbb3e5b1a24a42809043c1c0585b9
hais-hbh <578092215@qq.com>
2021-06-06 16:35:32 +0800

    创建 button-group

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from  './icon.vue'
+import ButtonGroup from './zero-button-group.vue'
 Vue.component('zero-button',Button)
 Vue.component('zero-icon',Icon)
+Vue.component('zero-button-group',ButtonGroup)
 new Vue({
  el:'#app',
  data:{
