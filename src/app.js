28710ff5fcca9bf5816ec8febdb5a5334cd7c5b2
hais-hbh <578092215@qq.com>
2021-06-07 18:57:20 +0800

    基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,12 +5,22 @@ import ButtonGroup from './zero-button-group.vue'
 import Input from './input.vue'
 import Row from './row.vue'
 import Col from './col.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Sider from './sider.vue'
+import Content from './content.vue'
+import Footer from './footer.vue'
 Vue.component('zero-button',Button)
 Vue.component('zero-icon',Icon)
 Vue.component('zero-button-group',ButtonGroup)
 Vue.component('zero-input',Input)
 Vue.component('zero-row',Row)
 Vue.component('zero-col',Col)
+Vue.component('zero-header',Header)
+Vue.component('zero-footer',Footer)
+Vue.component('zero-content',Content)
+Vue.component('zero-sider',Sider)
+Vue.component('zero-layout',Layout)
 new Vue({
  el:'#app',
  data:{
