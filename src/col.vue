5202064bb4700075878bd6b385f25bec3113081d
hais-hbh <578092215@qq.com>
2021-06-07 15:57:17 +0800

    将 phone 作为默认样式

--- src/col.vue
+++ src/col.vue
@@ -1,79 +1,76 @@
 <template>
-<div class="col" :class="colClass"
-     :style="colStyle"
->
-  <slot></slot>
-</div>
+  <div class="col" :class="colClass"
+       :style="colStyle"
+  >
+    <slot></slot>
+  </div>
 </template>
 <script>
-let validator = (value)=>{
+let validator = (value) => {
   let keys = Object.keys(value)
   let valid = true
-  keys.forEach((key)=>{
-    if(!['span','offset'].includes(key)){
+  keys.forEach((key) => {
+    if (!['span', 'offset'].includes(key)) {
       valid = false
     }
   })
   return valid
 }
 export default {
-  name:'zero-col',
-  props:{
-    span:{
-      type:[Number,String]
+  name: 'zero-col',
+  props: {
+    span: {
+      type: [Number, String]
     },
-    offset:{
-      type:[Number,String]
+    offset: {
+      type: [Number, String]
     },
-    phone:{
-      type:Object,
+
+    ipad: {
+      type: Object,
       validator,
     },
-    ipad:{
-      type:Object,
+    narrowPc: {
+      type: Object,
       validator,
     },
-    narrowPc:{
-      type:Object,
+    pc: {
+      type: Object,
       validator,
     },
-    pc:{
-      type:Object,
-      validator,
-    },
-    widePc:{
-      type:Object,
+    widePc: {
+      type: Object,
       validator,
     }
 
   },
-  data(){
+  data() {
     return {
       gutter: 0,
     }
   },
-  computed:{
-    colStyle(){
+  computed: {
+    colStyle() {
       return {
-        paddingLeft: this.gutter/2+'px',
-        paddingRight: this.gutter/2+'px'
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
       }
     },
-    colClass(){
-      let {span,offset,phone,ipad,narrowPc,pc,widePc} =this
-      return [span&&`col-${span}`,offset&&`offset-${offset}`,
-        ...(phone&&[`col-phone-${phone.span}`]),
-        ...(ipad&&[`col-ipad-${ipad.span}`]),
-        ...(narrowPc&&[`col-narrow-pc-${narrowPc.span}`]),
-        ...(pc&&[`col-pc-${pc.span}`]),
-        ...(widePc&&[`col-wide-pc-${widePc.span}`]),
+    colClass() {
+      let {span, offset, ipad, narrowPc, pc, widePc} = this
+      return [span && `col-${span}`, offset && `offset-${offset}`,
+
+        ...(ipad && [`col-ipad-${ipad.span}`]),
+        ...(narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+        ...(pc && [`col-pc-${pc.span}`]),
+        ...(widePc && [`col-wide-pc-${widePc.span}`]),
       ]
     }
   },
 }
 </script>
 <style lang="scss" scoped>
-.col{
+.col {
   $class-prefix: col- ;
 
   @for $n from 1 through 24 {
@@ -87,22 +84,8 @@ export default {
       margin-left: ($n/24)*100%;
     }
   }
-  @media (max-width: 576px) {
-    $class-prefix: col-phone- ;
 
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        width: ($n/24)*100%;
-      }
-    }
-    $class-prefix: offset-phone- ;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        margin-left: ($n/24)*100%;
-      }
-    }
-  }
-  @media (min-width:577px) and (max-width: 768px) {
+  @media (min-width: 577px) and (max-width: 768px) {
     $class-prefix: col-ipad- ;
 
     @for $n from 1 through 24 {
@@ -117,7 +100,7 @@ export default {
       }
     }
   }
-  @media (min-width:769px) and (max-width: 992px) {
+  @media (min-width: 769px) and (max-width: 992px) {
     $class-prefix: col-narrow-pc- ;
 
     @for $n from 1 through 24 {
@@ -132,7 +115,7 @@ export default {
       }
     }
   }
-  @media (min-width:993px) and (max-width: 1200px) {
+  @media (min-width: 993px) and (max-width: 1200px) {
     $class-prefix: col-pc- ;
 
     @for $n from 1 through 24 {
