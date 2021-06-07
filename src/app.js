770b2da42da206333829a095f3d32bfc171edf80
hais-hbh <578092215@qq.com>
2021-06-07 13:08:45 +0800

    row col 雏形完成

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import Button from './button.vue'
 import Icon from  './icon.vue'
 import ButtonGroup from './zero-button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 Vue.component('zero-button',Button)
 Vue.component('zero-icon',Icon)
 Vue.component('zero-button-group',ButtonGroup)
 Vue.component('zero-input',Input)
+Vue.component('zero-row',Row)
+Vue.component('zero-col',Col)
 new Vue({
  el:'#app',
  data:{
